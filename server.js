408abf4db719e8b6066115ad865fc376d912842d
Dima <dima1034@users.noreply.github.com>
2017-11-01 16:10:02 +0200

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+const jsonServer = require('json-server');
+const server = jsonServer.create();
+const middlewares = jsonServer.defaults();
+
+const path = require('path')
+const router = jsonServer.router(path.join(__dirname, 'db.json'))
+
+server.use(middlewares);
+server.use(router);
+server.listen(process.env.PORT || 3000, () => {
+  console.log('JSON Server is running');
+});
